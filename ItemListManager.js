592b9393c370056d88dd6364ca24b99d75572b82
priya-dharshini-n <priyabesonatech@gmail.com>
2024-12-11 17:26:46 +0530

    Create ItemListManager.js

---
+++ ItemListManager.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "h8k-components";
+
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddItem = () => {
+    
+    // if(input.trim().length === 0) {
+    //   return;
+    // }
+
+    setItems((val) => [...val, input])
+    setInput("")
+  };
+
+  return (
+    <>
+      <h8k-navbar header="Item List Manager"></h8k-navbar>
+      <div className="App">
+        <h3>Item List</h3>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter item"
+          data-testid="input-field"
+        />
+        <button onClick={handleAddItem} data-testid="add-button" disabled={input == "" ? true : false}>
+          Add Item
+        </button>
+        <ul data-testid="item-list">
+          {items.map((item, index) => (
+            <li key={index} data-testid="list-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
